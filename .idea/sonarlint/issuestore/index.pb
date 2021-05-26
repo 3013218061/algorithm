0b095ee3f1ab34b23bcc644400200ac2af9229be
shangjinyu <shangjinyu@meituan.com>
2021-05-26 14:27:21 +0800

    LongestSubstringWithoutRepeatingCharacters

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-R
-"src/main/java/org/example/App.java,2/6/268b09d2a5cdab7d148f0ec91f7b3fb51601410d
 U
 %src/main/java/org/example/TwoSum.java,b/d/bd30dc106c9b8a901f635e8d909c5737e0f828ae
 V
--- src/main/java/org/example/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/org/example/LongestSubstringWithoutRepeatingCharacters.java
@@ -10,23 +10,20 @@ import java.util.Set;
  **/
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
-        int result = 0;
+        int result = 0, start = 0, end = 0;
         if(s == null || s.length() < 1){
             return result;
         }
-        int start = 0, end = 0;
         Set<Character> charSet = new HashSet<>();
         while (true){
             while (!charSet.contains(s.charAt(end))){
-                charSet.add(s.charAt(end));
-                end++;
+                charSet.add(s.charAt(end++));
                 result = Math.max(result,charSet.size());
                 if(end == s.length()){
                     return result;
                 }
             }
-            charSet.remove(s.charAt(start));
-            start++;
+            charSet.remove(s.charAt(start++));
         }
     }
 }
